a9e2e7394bafde23ffc4f34e304f39eb2ef13800
jomcgi <joe.mcginley2@gmail.com>
2022-01-25 11:14:46 +0000

    removing old commits

---
+++ old/commit_web_changes.sh
@@ -0,0 +1,7 @@
+Rscript '--args UK' /mnt/disks/dev-joe-data/repos/ClearSkies/src/updateMap.R
+
+git config --global user.email "joe.mcginley2@gmail.com"
+git config --global user.name "jomcgi"
+git commit . -m 'map files updated'
+
+
